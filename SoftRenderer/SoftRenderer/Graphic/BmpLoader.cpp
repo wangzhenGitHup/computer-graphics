2598fea476c8721dd3e66beaed3514b87028695a
wangzhen <463208406@qq.com>
2019-09-16 18:16:42 +0800

    添加解析bmp图片格式库

---
+++ SoftRenderer/SoftRenderer/Graphic/BmpLoader.cpp
@@ -0,0 +1,73 @@
+#include "BmpLoader.h"
+#include <stdio.h>
+
+BmpLoader::BmpLoader()
+{
+	_header = new unsigned char[54];
+}
+
+BmpLoader::~BmpLoader()
+{
+	delete[] _header;
+	delete[] _data;
+}
+
+int BmpLoader::getWidth()
+{
+	return _width;
+}
+
+int BmpLoader::getHeight()
+{
+	return _height;
+}
+
+bool BmpLoader::loadBitmap(const char* fileName)
+{
+	FILE* pFile = fopen(fileName, "rb");
+	if (!pFile)
+	{
+		printf("Image %s could not be opened\n", fileName);
+		return false;
+	}
+
+	//文件头并非54字节，读取失败
+	if (fread(_header, 1, 54, pFile) != 54)
+	{
+		printf("Not a correct BMP file\n");
+		return false;
+	}
+
+	//文件头开头并非BM，读取失败
+	if (_header[0] != 'B' || _header[1] != 'M')
+	{
+		printf("Not a correct BMP file\n");
+		return false;
+	}
+
+	_dataPos =   *(int*)&(_header[0X0A]); //读取位置，位置在文件头0X0A处
+	_imageSize = *(int*)&(_header[0X22]); //图片内容大小数据，位置在文件头0X22处
+	_width =     *(int*)&(_header[0X12]); //图片宽度数据，位置在文件头0X12处
+	_height =    *(int*)&(_header[0X16]); //图片高度数据，位置在文件头0X16处
+
+	if (_imageSize == 0)
+	{
+		_imageSize = _width * _height * 3; //图片内容数据 == 总像素x3
+	}
+	
+	if (_dataPos == 0)
+	{
+		_dataPos = 54; //文件头读完，位置在54字节处
+	}
+
+	_data = new unsigned char[_imageSize]; //data放像素信息
+	fread(_data, 1, _imageSize, pFile); //读取像素
+	fclose(pFile);
+
+	for (int i = 0; i < (int)_imageSize; i += 3)
+	{
+		swapColor(_data[i], _data[i + 2]);
+	}
+
+	return true;
+}
---
+++ SoftRenderer/SoftRenderer/Graphic/BmpLoader.h
@@ -0,0 +1,32 @@
+#ifndef  __BMPLOADER__H__
+#define  __BMPLOADER__H__
+
+#define BLUIE 0 
+#define GREEN 1
+#define RED   2
+
+#define swapColor(a, b) {\
+	(a) ^= (b); \
+	(b) ^= (a); \
+	(a) ^= (b); \
+}
+
+class BmpLoader
+{
+public:
+	BmpLoader();
+	~BmpLoader();
+	int getWidth();
+	int getHeight();
+	unsigned char* getData(){ return _data; }
+	bool loadBitmap(const char* fileName);
+	
+private:
+	unsigned char* _header; //文件头
+	unsigned int _dataPos; //读取位置
+	unsigned int _imageSize; //图片内容大小
+	unsigned char* _data; //图片内容rgb
+	unsigned int _width, _height; //图片宽高
+};
+
+#endif
